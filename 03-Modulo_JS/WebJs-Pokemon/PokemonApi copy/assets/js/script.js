f1e34e4ee52f61adfa155713df28f19b67e4aeb1
Clairtonlm <clairtonlm@gmail.com>
2023-09-02 23:38:12 -0300

    Integrando API

---
+++ 03-Modulo_JS/WebJs-Pokemon/PokemonApi copy/assets/js/script.js
@@ -0,0 +1,11 @@
+//como fazer requisição http via javascript
+const url = 'https://pokeapi.co/'
+const linit = 10;
+const ofSet = 0;
+
+fetch(url).then(response=>{
+    console.log(response)
+})
+
+const x = 10 + 10
+console.log(x);
