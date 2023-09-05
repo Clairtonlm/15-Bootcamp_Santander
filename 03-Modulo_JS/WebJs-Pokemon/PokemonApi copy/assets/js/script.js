f3204e3642b17684d2b074b6239d5fc843dc75b8
Clairtonlm <clairtonlm@gmail.com>
2023-09-04 22:12:48 -0300

    Interação de pOkemons

--- 03-Modulo_JS/WebJs-Pokemon/PokemonApi copy/assets/js/script.js
+++ 03-Modulo_JS/WebJs-Pokemon/PokemonApi copy/assets/js/script.js
@@ -4,19 +4,52 @@ const limit = 10;
 const offset = 0;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+function convertpokemonToHtml(pokemon){
+    return `
+    <li class="pokemon">
+    <span class="number">#001</span>
+    <span class="name">${pokemon.name}</span>
+    <div class="detail">
+        <ol class="types">
+            <li class="type">grass</li>
+            <li class="type">poison</li>
+        </ol>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/5.svg" alt="${pokemon.name}" srcset="">
+    </div>
+    
+</li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
 fetch(url)
-.then((response) =>{
-     return response.json()
+.then((response)=>response.json())
+.then((jsonBody)=> jsonBody.results)
+.then((pokemons)=>{
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        pokemonList.innerHTML += convertpokemonToHtml(pokemon)
+        
+    }
 })
-.then((jsonBody)=>{
-    console.log(jsonBody);
-})
-.catch(function(error){
-    console.error('deu erro na req')
-})
-.finally(function(){
-    console.log("Requisição concluída")
-})
+.catch((error)=> console.log(error))
+
+
+
+// fetch(url)
+// .then((response) =>{
+//      return response.json()
+// })
+// .then((jsonBody)=>{
+//     console.log(jsonBody);
+// })
+// .catch(function(error){
+//     console.error('deu erro na req')
+// })
+// .finally(function(){
+//     console.log("Requisição concluída")
+// })
 
 
 // fetch(url)
