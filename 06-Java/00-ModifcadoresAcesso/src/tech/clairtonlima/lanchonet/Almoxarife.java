fd404e6dd3e41517192e5ffa887729e5448713da
Clairtonlm <clairtonlm@gmail.com>
2023-09-06 21:42:53 -0300

    Modificadores de acesso

---
+++ 06-Java/00-ModifcadoresAcesso/src/tech/clairtonlima/lanchonet/Almoxarife.java
@@ -0,0 +1,5 @@
+package tech.clairtonlima.lanchonet;
+
+public class Almoxarife {
+
+}
---
+++ 06-Java/00-ModifcadoresAcesso/src/tech/clairtonlima/lanchonet/Estabelecimento.java
@@ -0,0 +1,11 @@
+package tech.clairtonlima.lanchonet;
+
+public class Estabelecimento {
+	
+	public void escolherLanche() {
+		System.out.println("escolher lanche");
+	}
+	public void fazerpedido() {
+		System.out.println("fazendo pedido");
+	}
+}
